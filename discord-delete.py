db36b55d40936da4636f482291d7608a4662bd00
Connor E <38229097+c-edw@users.noreply.github.com>
2019-02-22 04:51:51 +0000

    Further code refactor. It's a lot more solid now.

--- discord-delete.py
+++ discord-delete.py
@@ -3,6 +3,7 @@ import asyncio
 import os
 import csv
 
+from aiohttp import ClientResponseError
 from csv import DictReader
 
 API = "https://discordapp.com/api/v6"
@@ -16,21 +17,24 @@ class Discord:
         await self.session.close()
 
     async def _req(self, method, endpoint, body=None):
-        async with self.session.request(
-                method, "{}/{}".format(API, endpoint), 
-                headers={ "Authorization": self.token },
-                json=body) as resp:
+        url = "{}/{}".format(API, endpoint)
+        headers = { "Authorization": self.token }
+
+        async with self.session.request(method, url,
+                headers=headers, json=body) as resp:
             try:
-                data = await resp.json()
+                json = await resp.json()
+            except ClientResponseError:
+                # If we're being rate limited, wait for a while.
+                if resp.status == 429:
+                    await asyncio.sleep(json.get("retry_after") / 1000)
+                    return await self._req(method, endpoint, body)
+
+                return {}
             except Exception:
-                return None
+                raise
 
-            # If we're being rate limited, wait for a while.
-            if resp.status == 429:
-                await asyncio.sleep(data["retry_after"] / 1000)
-                return await self._req(method, endpoint, body)
-
-            return data
+            return json
 
     async def me(self):
         return await self._req("GET", "/users/@me")
@@ -38,22 +42,24 @@ class Discord:
     async def relationships(self):
         return await self._req("GET", "/users/@me/relationships")
 
-    async def relationship_channels(self, body=None):
-        return await self._req("POST", "/users/@me/channels", body)
+    async def relationship_channels(self, rid):
+        return await self._req("POST", "/users/@me/channels", {
+            "recipients": [rid]
+        })
 
     async def guilds(self):
         return await self._req("GET", "/users/@me/guilds")
 
-    async def guild_messages(self, gid, aid):
+    async def guild_msgs(self, gid, aid):
         return await self._req("GET", "/guilds/{}/messages/search?author_id={}&include_nsfw=true&limit=25".format(gid, aid))
 
     async def channels(self):
         return await self._req("GET", "/users/@me/channels")
 
-    async def channel_messages(self, cid, aid):
+    async def channel_msgs(self, cid, aid):
         return await self._req("GET", "/channels/{}/messages/search?author_id={}&include_nsfw=true&limit=25".format(cid, aid))
 
-    async def delete_message(self, cid, mid):
+    async def delete_msg(self, cid, mid):
         return await self._req("DELETE", "/channels/{}/messages/{}".format(cid, mid))
 
 async def delete_from_current(discord):
@@ -62,54 +68,60 @@ async def delete_from_current(discord):
     Delete messages from the conversations and servers the user currently participates in.
 
     """
-    meid = (await discord.me())["id"]
+    meid = (await discord.me()).get("id")
 
     channels = await discord.channels()
-    cids = [channel["id"] for channel in channels]
+    cids = [channel.get("id") for channel in channels]
+
     for cid in cids:
         await delete_from_channel(discord, meid, cid)
 
     relations = await discord.relationships()
-    rids = [relation["id"] for relation in relations]
-    for rid in rids:
-        channel = await discord.relationship_channels({
-            "recipients": [rid]
-        })
+    rids = [relation.get("id") for relation in relations]
 
-        if not channel["id"] in cids:
-            await delete_from_channel(discord, meid, channel["id"])
+    for rid in rids:
+        channel = await discord.relationship_channels(rid)
+
+        if not channel.get("id") in cids:
+            await delete_from_channel(discord, meid, channel.get("id"))
 
     guilds = await discord.guilds()
-    gids = [guild["id"] for guild in guilds]
+    gids = [guild.get("id") for guild in guilds]
+
     for gid in gids:
         await delete_from_guild(discord, meid, gid)
 
 async def delete_from_channel(discord, meid, cid):
     print("Deleting messages in channel {}...".format(cid))
 
-    messages = await discord.channel_messages(cid, meid)
+    results = True
+    while results:
+        mlist = await discord.channel_msgs(cid, meid)
+        await delete_msgs(discord, meid, mlist.get("messages"))
 
-    while messages.get("total_results"):
-        await delete_messages(discord, meid, messages)
-        messages = await discord.channel_messages(cid, meid)
+        results = mlist.get("total_results")
 
 async def delete_from_guild(discord, meid, gid):
     print("Deleting messages in guild {}...".format(gid))
+    
+    results = True
+    while results:
+        mlist = await discord.guild_msgs(gid, meid)
+        await delete_msgs(discord, meid, mlist.get("messages"))
 
-    messages = await discord.guild_messages(gid, meid)
+        results = mlist.get("total_results")
 
-    while messages.get("total_results"):
-        await delete_messages(discord, meid, messages)
-        messages = await discord.guild_messages(gid, meid)
-
-async def delete_messages(discord, meid, messages):
-    for context in messages["messages"]:
-        for message in context:
-            if not message.get("hit"):
+async def delete_msgs(discord, meid, msgs):
+    for context in msgs:
+        for msg in context:
+            if not msg.get("hit"):
                 continue
 
-            print("Deleted message {}.".format(message["id"]))
-            await discord.delete_message(message["channel_id"], message["id"])
+            print("\t- {}".format(msg.get("id")))
+            await discord.delete_msg(
+                msg.get("channel_id"), 
+                msg.get("id")
+            )
 
 async def delete_from_all(discord):
     """
@@ -124,11 +136,11 @@ async def delete_from_all(discord):
         if not os.path.isdir("./messages/{}".format(filename)):
             return
 
-        messages = open("./messages/{}/messages.csv".format(filename), "r")
-        reader = DictReader(messages)
+        msgs = open("./messages/{}/messages.csv".format(filename), "r")
+        reader = DictReader(msgs)
 
         for line in reader:
-            await discord.delete_message(filename, line["ID"])
+            await discord.delete_msg(filename, line["ID"])
 
 async def main():
     token = os.environ.get("DISCORD_TOKEN")
